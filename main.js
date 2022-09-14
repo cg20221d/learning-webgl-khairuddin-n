f387a559d9d8c0bd87865ca9b2a3f7f996293e32
Khairuddin Nasty <nastykhairuddin@gmail.com>
2022-09-14 15:11:05 +0700

    Gambar 3 titik

--- main.js
+++ main.js
@@ -2,11 +2,22 @@ function main() {
   var kanvas = document.getElementById("kanvas");
   var gl = kanvas.getContext("webgl");
 
+  var vertices = [
+    0.5, 0.5, // A: kanan atas
+    0.0, 0.0, // B: kanan bawah
+    -0.5, 0.5 // C: kiri atas
+  ];
+
+  var buffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
   // Vertex shader
   var vertexShaderCode =  `
+  attribute vec2 aPosition;
   void main() {
-      float x = 0.0;
-      float y = 0.0;
+      float x = aPosition.x;
+      float y = aPosition.y;
       gl_PointSize = 10.0;
       gl_Position = vec4(x, y, 0.0, 1.0);
   }
@@ -35,9 +46,16 @@ function main() {
   gl.linkProgram(shaderProgram);
   gl.useProgram(shaderProgram);
 
+  // Mengajari GPU bagaimana cara mengoleksi 
+  // nilai posisi dari ARRAY_BUFFER
+  // untuk setiap verteks yang sedang diproses
+  var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
+  gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(aPosition);
+
   gl.clearColor(1.0,      0.65,    0.0,    1.0);  // Oranye
   //            Merah     Hijau   Biru    Transparansi
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  gl.drawArrays(gl.POINTS, 0, 1);
+  gl.drawArrays(gl.POINTS, 0, 3);
 }
