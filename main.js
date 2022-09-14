e46fcd37edd717242776ac9aa6540b5c2f405021
Khairuddin Nasty <nastykhairuddin@gmail.com>
2022-09-14 14:51:26 +0700

    Gambar satu titik

--- main.js
+++ main.js
@@ -1,31 +1,43 @@
 function main() {
-    var kanvas = document.getElementById("kanvas");
-    var gl = kanvas.getContext("webgl");
-  
-    // Vertex shader
-    var vertexShaderCode = "void main() {" + "}";
-  
-    var vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vertexShaderObject, vertexShaderCode);
-    gl.compileShader(vertexShaderObject); // sampai sini sudah jadi .o
-  
-    // Fragment shader
-    var fragmentShaderCode = `
-    void main() {
-    }
-    `;
-  
-    var fragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fragmentShaderObject, fragmentShaderCode);
-    gl.compileShader(fragmentShaderObject); // sampai sini sudah jadi .o
-  
-    var shaderProgram = gl.createProgram(); // wadah dari executable (.exe)
-    gl.attachShader(shaderProgram, vertexShaderObject);
-    gl.attachShader(shaderProgram, fragmentShaderObject);
-    gl.linkProgram(shaderProgram);
-    gl.useProgram(shaderProgram);
-  
-    gl.clearColor(1.0, 0.65, 0.0, 1.0);
-    //            red green blue alpha
-    gl.clear(gl.COLOR_BUFFER_BIT);
+  var kanvas = document.getElementById("kanvas");
+  var gl = kanvas.getContext("webgl");
+
+  // Vertex shader
+  var vertexShaderCode =  `
+  void main() {
+      float x = 0.0;
+      float y = 0.0;
+      gl_PointSize = 10.0;
+      gl_Position = vec4(x, y, 0.0, 1.0);
   }
+  `;
+  var vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
+  gl.shaderSource(vertexShaderObject, vertexShaderCode);
+  gl.compileShader(vertexShaderObject);   // sampai sini sudah jadi .o
+
+  // Fragment shader
+  var fragmentShaderCode = `
+  precision mediump float;
+  void main() {
+      float r = 0.0;
+      float g = 0.0;
+      float b = 1.0;
+      gl_FragColor = vec4(r, g, b, 1.0);
+  }
+  `;
+  var fragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER);
+  gl.shaderSource(fragmentShaderObject, fragmentShaderCode);
+  gl.compileShader(fragmentShaderObject);   // sampai sini sudah jadi .o
+
+  var shaderProgram = gl.createProgram(); // wadah dari executable (.exe)
+  gl.attachShader(shaderProgram, vertexShaderObject);
+  gl.attachShader(shaderProgram, fragmentShaderObject);
+  gl.linkProgram(shaderProgram);
+  gl.useProgram(shaderProgram);
+
+  gl.clearColor(1.0,      0.65,    0.0,    1.0);  // Oranye
+  //            Merah     Hijau   Biru    Transparansi
+  gl.clear(gl.COLOR_BUFFER_BIT);
+
+  gl.drawArrays(gl.POINTS, 0, 1);
+}
